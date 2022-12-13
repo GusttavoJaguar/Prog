6e4ed70c02dc88aecdd385a32b1dd28bc568c54a
GUSTAVO <71243629+GusttavoJaguar@users.noreply.github.com>
2022-12-13 11:10:19 -0300

    Update adivinhe.py

--- PY/adivinhe.py
+++ PY/adivinhe.py
@@ -2,22 +2,22 @@ from random import randint
 n  = int (randint(1,10))
 p = 0
 t = 0
-print ("----------ADIVINHE----------")
-
+print ("         ----------ADIVINHE O NÚMERO----------")
+print ("")
 sair = False
 
 while sair == False:
 
-while p != n:
-	p = int(input("Chute um número de 1 à 10: 10"))
-	t += 1
-	if p == n:
-		print("ACERTOU!!! Placar %i" % t)
-	elif p < n:
-		print("Chute um valor maior")
-	else:
-		print("Chute um valor menor")
+  while p != n:
+  	p = int(input("Chute um número de 1 à 100: "))
+  	t += 1
+  	if p == n:
+  		print("ACERTOU!!! Placar %i" % t)
+  	elif p < n:
+  		print("Chute um valor maior")
+  	else:
+  		print("Chute um valor menor")
 
 acao = input("Deseja sair do jogo? (S/N) ")
-	if acao == "s"
+if acao == "s":
 	sair = True
